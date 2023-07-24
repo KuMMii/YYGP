c79cb4933b640c25949fb5609ba48b6324bd4831
KuMMii <4.prgmng@gmail.com>
2023-07-24 17:20:53 +0900

    14696

---
+++ [0727] 01. Algorithm/BOJ_17413. 단어 뒤집기 2/BJ_17413.java
@@ -0,0 +1,18 @@
+package YYP.bj_17413;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_17413 {
+	
+	public static String str;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		str=br.readLine();
+		
+		String[] strSp=str.split("<");
+	}
+}

de033d9b02ad1b3d4d1a765e482ffa4e42541f05
KuMMii <4.prgmng@gmail.com>
2023-07-25 14:27:10 +0900

    17413 hateu

--- [0727] 01. Algorithm/BOJ_17413. 단어 뒤집기 2/BJ_17413.java
+++ [0727] 01. Algorithm/BOJ_17413. 단어 뒤집기 2/BJ_17413.java
@@ -3,16 +3,52 @@ package YYP.bj_17413;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BJ_17413 {
 	
-	public static String str;
 	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		str=br.readLine();
 		
-		String[] strSp=str.split("<");
+		String[] strSp=br.readLine().split("");
+		List<String> sent=new ArrayList<String>();
+		for(int i=0; i<strSp.length;i++) {
+			String str="";
+			if(strSp[i]=="<") {
+				while(strSp[i]==">") {
+					str+=strSp[i];
+				}
+				sent.add(str);
+			}else if(strSp[0]!="<") {
+				int cnt=0;
+				for(int j=0; j<strSp.length;j++) {
+					if(strSp[i]=="<") cnt++;
+				}
+				
+				if(cnt==0) {
+					while(strSp[i]=="<") {
+						str+=strSp[i];
+					}
+					
+				}
+				while(strSp[i]=="<") {
+					str+=strSp[i];
+				}
+				StringBuilder sb = new StringBuilder(str);
+				sent.add(sb.reverse().toString());
+			}else if(strSp[i-1]==">") {
+				while(strSp[i+1]=="<"||strSp[i+1]=="") {
+					str+=strSp[i];
+				}
+				StringBuilder sb = new StringBuilder(str);
+				sent.add(sb.reverse().toString());
+			}
+		}
+		for(String s:sent) {
+			System.out.print(s);
+		}
 	}
 }

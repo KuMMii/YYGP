56834e383cf9a9d2a354a07440828fa2bba32499
KuMMii <4.prgmng@gmail.com>
2023-07-26 13:57:46 +0900

    finally!!!!!!

--- [0727] 01. Algorithm/PG_134240. 푸드 파이트 대회/PG_134240.java
+++ [0727] 01. Algorithm/PG_134240. 푸드 파이트 대회/PG_134240.java
@@ -6,32 +6,29 @@ public class PG_134240 {
 class Solution {
     public String solution(int[] food) {
         String answer = "";
-        int one=0;
-        int two=0;
-        int three=0;
         
-        if(food[1]!=0) {one=(int)food[1]/2;}
-        if(food[2]!=0) {two=(int)food[2]/2;}
-        if(food[3]!=0) {three=(int)food[3]/2;}
+        
+        int[] idx=new int[food.length-1];
+        for(int i=1; i<food.length;i++) {
+        	idx[i-1]=food[i];
+        	
+        	if(idx[i-1]!=0) {
+        		for(int j=0;j<(int)idx[i-1]/2;j++) {
+        			answer+=(i+"");
+        		}
+        	}
+        }
         String rev="";
-        
-        for(int i=0;i<one;i++){
-            answer+="1";
-        }
-        for(int i=0;i<two;i++){
-            answer+="2";
-        }
-        for(int i=0;i<three;i++){
-            answer+="3";
-        }
         
         for(int i=0; i<answer.length();i++){
             rev=answer.charAt(i)+rev;
         }
         answer+="0"+rev;
         
-        
-        
-        return answer;
+        if(answer.length()>=3) {
+        	
+        	return answer;
+        }
+        return null;
     }
 }

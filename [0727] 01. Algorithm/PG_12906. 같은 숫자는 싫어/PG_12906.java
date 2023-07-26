30decabd4506ee56929e565e9330f9d91dbf27e2
jy_is0206 <jy_is0206@naver.com>
2023-07-26 17:41:24 +0900

    230726 commit

---
+++ [0727] 01. Algorithm/PG_12906. 같은 숫자는 싫어/PG_12906.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PG_12906 {
+	    public int[] solution(int[] arr) {
+	        
+	        Queue<Integer> q = new LinkedList<>();
+	        q.offer(arr[0]);
+	        int num = 1;
+	        
+	        for (int i = 1; i<arr.length; i++) {
+	            if (arr[i] != arr[i - 1]) {
+	                q.offer(arr[i]);
+	                num++;
+	            }
+	        }
+
+	        int[] answer = new int[num];
+	        for (int i = 0; i<num; i++) {
+	                answer[i] = q.poll();
+	        }
+	        
+	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+//	        System.out.println(Arrays.toString(answer));
+
+	        return answer;
+	    }
+}

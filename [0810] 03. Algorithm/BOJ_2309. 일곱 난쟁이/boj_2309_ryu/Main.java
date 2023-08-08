b3f60b9897a60558084b24c954a6dabaea07a53e
Kihyun Ryu <khryu0610@gmail.com>
2023-08-08 17:48:18 +0900

    Create: day03

--- [0810] 03. Algorithm/BOJ_2309. 일곱 난쟁이/boj_2309_ryu/Main.java
+++ [0810] 03. Algorithm/BOJ_2309. 일곱 난쟁이/boj_2309_ryu/Main.java
@@ -1,3 +1,5 @@
+package boj_2309_ryu;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
--- [0810] 03. Algorithm/BOJ_2789. 블랙잭/boj_2789_ryu/Main.java
+++ [0810] 03. Algorithm/BOJ_2789. 블랙잭/boj_2789_ryu/Main.java
@@ -1,3 +1,5 @@
+package boj_2789_ryu;
+
 import java.util.Scanner;
 
 public class Main {

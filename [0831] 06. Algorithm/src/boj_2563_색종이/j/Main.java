ebd7d72d51d7490faf3b33746f453f1b4b01b618
KuMMii <4.prgmng@gmail.com>
2023-08-29 20:06:40 +0900

    success russia

--- [0831] 06. Algorithm/src/boj_2563_색종이/j/Main.java
+++ [0831] 06. Algorithm/src/boj_2563_색종이/j/Main.java
@@ -1,10 +1,47 @@
-package boj_2563_색종이.j;
+package BOJ_2563;
 
+import java.util.Scanner;
+
+//3개 겹쳤을 때 안됨
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		
+		int[][] arr=new int[N][4];
+		
+		int sum=0;
+		
+		//{가로 시작, 가로 끝, 세로 시작, 세로 끝}
+		for(int n=0; n<N; n++) {
+			arr[n][0]=sc.nextInt();
+			arr[n][1]=arr[n][0]+10;
+			arr[n][2]=sc.nextInt();
+			arr[n][3]=arr[n][2]+10;
+		}//for n
+		
+		for(int i=0; i<N; i++) {// 비교 지정
+			for(int j=0; j<N; j++) {//다 돌림
+				if(i!=j) {
+					
+					if((arr[i][0]<=arr[j][0] && arr[j][0]<arr[i][1]) &&
+							((arr[i][2]<=arr[j][3] && arr[j][3]<arr[i][3]) ||
+							(arr[i][2]<=arr[j][2] && arr[j][2]<arr[i][3]))) {
+						sum+=(10-Math.abs(arr[j][0]-arr[i][0]))*(10-Math.abs(arr[i][3]-arr[j][3]));
+					}//if
+					
+					if((arr[i][0]<arr[j][1] && arr[j][1]<arr[i][1]) &&
+							((arr[i][2]<=arr[j][3] && arr[j][3]<arr[i][3]) ||
+							(arr[i][2]<=arr[j][2] && arr[j][2]<arr[i][3]))) {
+						
+						sum+=(10-Math.abs(arr[j][0]-arr[i][0]))*(10-Math.abs(arr[i][3]-arr[j][3]));
+					}//if
+					
+				}//if i!=j
+			}//for j
+		}//for i
+		
+		System.out.println(100*N-sum/2);
+		
+	}//main
+}//class
---
+++ [0831] 06. Algorithm/src/boj_2563_색종이/j/Main2.java
@@ -0,0 +1,45 @@
+package BOJ_2563;
+
+import java.util.Scanner;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		
+		int[][] rnc=new int[N][2];
+		int maxr=0;
+		int maxc=0;
+		
+		for(int n=0; n<N; n++) {
+			int c=sc.nextInt();
+			int r=sc.nextInt();
+			rnc[n][0]=r;
+			rnc[n][1]=c;
+			
+			maxc=Math.max(maxc, c);
+			maxr=Math.max(maxr, r);
+		}//for n
+
+		int[][] arr=new int[maxr+11][maxc+11];
+		
+		for(int i=0; i<N; i++) {
+			for(int r=rnc[i][0]; r<rnc[i][0]+10;r++) {
+				for(int c=rnc[i][1]; c<rnc[i][1]+10;c++) {
+					arr[r][c]=1;
+				}
+			}
+		}
+		
+		int cnt=0;
+		
+		for(int[] i : arr) {
+			for(int j : i) {
+				if(j==1) cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+		
+	}//main
+}//class

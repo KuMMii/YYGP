e47afba214cab9175ee228e3b54735fd80d3cd25
InSeungHwang <disparaitree@gmail.com>
2023-08-30 08:40:31 +0900

    230830 다음주 폴더 추가

---
+++ [0907] 07. Algorithm/src/boj_2477_참외밭/inseung/Main.java
@@ -0,0 +1,10 @@
+package boj_2477_참외밭.inseung;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ [0907] 07. Algorithm/src/boj_2567_색종이2/inseung/Main.java
@@ -0,0 +1,10 @@
+package boj_2567_색종이2.inseung;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

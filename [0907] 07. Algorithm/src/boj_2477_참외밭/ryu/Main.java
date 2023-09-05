fb39594023e3669668f0690887ec27d81a39ed10
Kihyun Ryu <khryu0610@gmail.com>
2023-09-05 23:14:12 +0900

    07

--- [0907] 07. Algorithm/src/boj_2477_참외밭/ryu/Main.java
+++ [0907] 07. Algorithm/src/boj_2477_참외밭/ryu/Main.java
@@ -1,10 +1,58 @@
 package boj_2477_참외밭.ryu;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Main {
+	static class Line {
+		int dir, len;
 
+		public Line(int dir, int len) {
+			super();
+			this.dir = dir;
+			this.len = len;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int K = sc.nextInt();
+		
+		int height = 0;
+		int width = 0;
+		Line[] lines = new Line[6];
+		
+		for (int i=0; i<6; i++) {
+			Line line = new Line(sc.nextInt(), sc.nextInt());
+			lines[i] = line; 
+			
+			if (line.dir==3 || line.dir==4) {
+				if (line.len > height) height = line.len;
+			} else {
+				if (line.len > width) width = line.len;
+			}
+		}
+		
+		int big, small;
+		// big
+		big = height * width;
+		// small
+		Pattern pattern = Pattern.compile("3131|4242|1414|2323");
+		String dirs = Arrays.stream(lines).map(line->String.valueOf(line.dir)).collect(Collectors.joining());
+		Matcher matcher = pattern.matcher(dirs+dirs);
+		int start, end;
+		matcher.find();
+		start = (matcher.start()+1) % 6;
+		end = (matcher.end()-2) % 6;
+		small = lines[start].len * lines[end].len;
+		
+		System.out.println((big-small)*K);
+		
+		sc.close();
 	}
 
 }
--- [0907] 07. Algorithm/src/boj_2567_색종이2/ryu/Main.java
+++ [0907] 07. Algorithm/src/boj_2567_색종이2/ryu/Main.java
@@ -1,10 +1,43 @@
 package boj_2567_색종이2.ryu;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] arr = new int[102][102];
+		int length = 10; //len of paper
+		int[] dr = {-1, 0, 1, 0};
+		int[] dc = {0, 1, 0, -1};
+		
+		int N = sc.nextInt(); //num of paper
+		for (int n=0; n<N; n++) {
+			int sy, sx, ey, ex;
+			sx = sc.nextInt()+1;
+			sy = sc.nextInt()+1;
+			ex = sx+length;
+			ey = sy+length;
+			
+			for (int r=sy; r<ey; r++) {
+				for (int c=sx; c<ex; c++) {
+					arr[r][c] = 1;
+				}
+			}
+		}
+		int ans = 0;
+		for (int r=0; r<arr.length; r++) {
+			for (int c=0; c<arr[0].length; c++) {
+				if (arr[r][c]==1) {
+					for (int i=0; i<4; i++) {
+						if (arr[r+dr[i]][c+dc[i]]==0) ans++;
+					}
+				}
+			}
+		}
+		System.out.println(ans);
+		sc.close();
 	}
 
 }

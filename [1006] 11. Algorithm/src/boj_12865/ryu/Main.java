ba065b1fc5b1f6723f3f56badd28e0505228a2fe
Kihyun Ryu <khryu0610@gmail.com>
2023-09-27 12:41:04 +0900

    [1006] 11.Algorithm 세팅

---
+++ [1006] 11. Algorithm/src/boj_12865/ryu/Main.java
@@ -0,0 +1,10 @@
+package boj_12865.ryu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ [1006] 11. Algorithm/src/boj_1970/j/Main.java
@@ -0,0 +1,10 @@
+package boj_1970.j;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

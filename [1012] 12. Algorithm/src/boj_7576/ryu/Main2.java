86b88d0243c6d5eeb4f8221ffbfc143dc1c29fe8
Kihyun Ryu <khryu0610@gmail.com>
2023-10-11 07:17:54 +0900

    tomato

--- [1012] 12. Algorithm/src/boj_7576/ryu/Main2.java
+++ [1012] 12. Algorithm/src/boj_7576/ryu/Main2.java
@@ -1,10 +1,11 @@
 package boj_7576.ryu;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 	static int[][] arr;
 	static int[][] visited;
 	static int R;
@@ -55,16 +56,16 @@ public class Main {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		
-		C = sc.nextInt();
-		R = sc.nextInt();
+		C = readInt();
+		R = readInt();
 		arr = new int[R][C];
 	
 		for (int r=0; r<R; r++) {
 			for (int c=0; c<C; c++) {
-				arr[r][c] = sc.nextInt();
+				arr[r][c] = readInt();
 			}
 		}
 		
@@ -86,4 +87,19 @@ public class Main {
 		
 		sc.close();
 	}
+    
+	//
+    static int readInt() throws IOException {
+        int n = 0;
+        boolean isNegative = false;
+        while (true) {
+            int input = System.in.read();
+            if (input<=32)
+                return isNegative ? n * -1 : n;
+            else if(input=='-')
+                isNegative = true;
+            else
+                n = (n<<3) + (n<<1) + (input&15);
+        }
+    }
 }
